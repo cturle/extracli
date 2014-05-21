31c77c8ab10ddbcd653e850aa3e1ab294a574eca
Christophe Turle <cturle@yahoo.com>
2014-05-21 10:10:33 +0200

    + kb: new-kb, exist?, add-concepts, add-properties

--- src/extracli/kb.clj
+++ src/extracli/kb.clj
@@ -2,10 +2,47 @@
   (:require [clojure.set :refer :all]) )
 
 
+
+; (new-kb)
+; (new-kb {1 {:isa :concept}, 2 {:isa :vector}})
+(defn new-kb
+  "returns a new knowledge base with INIT as initial concepts if provided.
+INIT is a map of concepts, where keys are numbers (ids) and values are map of properties."
+  ([]
+   {0 {:isa ::KB, :index-next-concept 1}} )
+  ([INIT]
+   (assoc INIT 0 {:isa ::KB, :index-next-concept (inc (apply max (keys INIT)))}) ))
+
+
+; (exist? (new-kb {1 {:isa :class, :pp1 1, :pp2 "pp2", :pp3 [1 2], :pp4 :other-property}}) {:isa :class, :pp1 1, :pp2 "pp2", :pp3 [1 2]})
 (defn exist?
   "returns true iff it exists an object in knowledge base KB1 with properties PPs"
   [KB PPs]
   (some (fn [C] (subset? (set PPs) (set C)))
         (vals KB) ))
+
+
+; (add-concepts (new-kb) 2)
+(defn add-concepts
+  "return a knowledge base and NB new ids. The knowledge base is the knowledge base KB0 where NB empty concepts have been added."
+  [KB0 NB]
+  (let [idx_pre  (get-in KB0 [0 :index-next-concept])
+        idx_post (+ idx_pre NB)
+        ids      (range idx_pre idx_post)
+        KB1      (assoc-in KB0 [0 :index-next-concept] idx_post)
+        KB_post  (reduce (fn [KB IDX] (assoc KB IDX {})) KB1 ids)]
+    (cons KB_post ids) ))
+
+
+; (add-properties (new-kb {1 {}}) 1 {:pp1 1})
+(defn add-properties
+  "adds properties to an existing concept."
+  [KB0 & C_PPs]
+  (reduce (fn [KB [idC PPs]]
+            (let [C_pre  (get KB idC)
+                  C_post (merge C_pre PPs)]
+              (assoc KB idC C_post) ))
+          KB0 (partition 2 C_PPs)) )
+
 
 
--- test/extracli/kb_test.clj
+++ test/extracli/kb_test.clj
@@ -2,8 +2,10 @@
   (:require [clojure.test :refer :all]
             [extracli.kb :refer :all] ))
 
+(def KB0 (new-kb {1 {:isa :class, :pp1 1, :pp2 "pp2", :pp3 [1 2], :pp4 :other-property}}))
+
 (deftest test-exist?
-  (let [KB {1 {:isa :class, :pp1 1, :pp2 "pp2", :pp3 [1 2], :pp4 :other-property}}]
+  (let [KB KB0]
     (testing "true cases"
       (is (exist? KB {:isa :class, :pp1 1, :pp2 "pp2", :pp3 [1 2]}))
       )
@@ -14,4 +16,36 @@
       (is (not (exist? KB {:isa :class, :pp1 1, :pp2 "pp2", :pp3 [1 :x]})))
       )
   ))
+
+
+(deftest test-add-concepts
+  (let [KB KB0]
+    (testing "cas nominal"
+      (let [NB          3
+            [KB2 & IDs] (add-concepts KB NB)]
+        (is (= NB (count IDs)))
+        (is (every? #(= {} (get KB2 %)) IDs))
+        ))))
+
+
+(deftest test-add-properties
+  (let [C1_pre  {:pp0 0}
+        C2_pre  {:pp2 0}
+        KB      (new-kb {1 C1_pre, 2 C2_pre})]
+    (testing "cas nominal"
+      (let [PPs1     {:pp1 1 :pp2 2}
+            PPs2     {:pp2 2 :pp3 3}
+            KB2      (add-properties KB, 1 PPs1, 2 PPs2)
+            C1_post  (get KB2 1)
+            C2_post  (get KB2 2)]
+        (is (= {:pp0 0 :pp1 1 :pp2 2} C1_post))
+        (is (= {:pp2 2 :pp3 3} C2_post))
+        ))))
+
+
+
+
+
+
+
 
